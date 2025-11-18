0db2dbb2de8a2d0b4147ad111088a64e29399524
mausamGurung67 <mausamgurung678@gmail.com>
2025-11-18 11:48:58 +0545

    Completed task 3 successfully

---
+++ Task3/ParameterDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class ParameterDemo
+    {
+        // Creating a method using ref parameter
+        public void Increase(ref int number)
+        {
+            number += 10;
+        }
+
+        // Creating a method using out parameter
+        public void GetFullName(out string fullname)
+        {
+            fullname = "Mausam Gurung";   // Assigning value to out parameter
+        }
+
+        // Creating a method using params keyword
+        public int SumAll(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object
+            ParameterDemo demo = new ParameterDemo();
+
+            // 1. Calling method with ref parameter
+            int value = 20;
+            Console.WriteLine("Before Increase: " + value);
+            demo.Increase(ref value);
+            Console.WriteLine("After Increase: " + value);
+
+            // 2. Calling method with out parameter
+            demo.GetFullName(out string fullName);
+            Console.WriteLine("Full Name: " + fullName);
+
+            // 3. Calling method with params
+            int total = demo.SumAll(5, 10, 15, 20);
+            Console.WriteLine("Sum of all numbers: " + total);
+
+            Console.ReadLine();
+        }
+    }
+}
