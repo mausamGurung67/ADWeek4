efbce6db15342494a386f5353d0a3972d3096ebd
mausamGurung67 <mausamgurung678@gmail.com>
2025-11-18 11:38:22 +0545

    Completed task 1 successfully

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating first object
+            Student s1 = new Student();
+            s1.name = "Mausam";
+            s1.age = 21;
+            s1.course = "Application Development";
+
+            // Creating second object
+            Student s2 = new Student();
+            s2.name = "Rajendra";
+            s2.age = 22;
+            s2.course = "Database";
+
+            // Displaying values for object 1
+            Console.WriteLine("Student 1 Details");
+            Console.WriteLine("Name: " + s1.name);
+            Console.WriteLine("Age: " + s1.age);
+            Console.WriteLine("Course: " + s1.course);
+
+            // Display values for object 2
+            Console.WriteLine("\nStudent 2 Details");
+            Console.WriteLine("Name: " + s2.name);
+            Console.WriteLine("Age: " + s2.age);
+            Console.WriteLine("Course: " + s2.course);
+
+            // Printing static field
+            Console.WriteLine("\nCollege Name (Static Field): " + Student.collegeName);
+
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Student
+    {
+        // Instance fields
+        public string name;
+        public int age;
+        public string course;
+
+        // Static field
+        public static string collegeName = "Itahari International College";
+    }
+}
