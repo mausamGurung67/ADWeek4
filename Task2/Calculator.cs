09d60065aa9e38b434d00b86d88b8101d58b3523
mausamGurung67 <mausamgurung678@gmail.com>
2025-11-18 11:44:12 +0545

    Completed task 2 successfully

---
+++ Task2/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Calculator
+    {
+        // Method to print welcome message
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Calculator");
+        }
+
+        // Method to add two numbers
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Method with optional parameter
+        public int Multiply(int num1, int num2 = 1)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating object
+            Calculator calc = new Calculator();
+
+            // Caling welcome method
+            calc.PrintWelcome();
+
+            // Calling Addition method
+            int sum = calc.Add(10, 20);
+            Console.WriteLine("Addition Result: " + sum);
+
+            // Calling Multiplication method with two parameters
+            int product1 = calc.Multiply(5, 4);
+            Console.WriteLine("Multiplication Result (5 * 4): " + product1);
+
+            // Calling Multiply method with default second parameter
+            int product2 = calc.Multiply(7);
+            Console.WriteLine("Multiplication Result (7 * default): " + product2);
+
+            Console.ReadLine();
+        }
+    }
+}
